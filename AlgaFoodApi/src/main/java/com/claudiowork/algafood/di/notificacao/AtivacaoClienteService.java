1a60264389ee84e7fa51b1d8acd40103551b6f80
Claudio <claudiosfcarvalho@outlook.com>
2020-07-05 20:12:30 -0300

    ambiguidade
    updating the consumer to accept multiple beans

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/di/notificacao/AtivacaoClienteService.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/di/notificacao/AtivacaoClienteService.java
@@ -1,15 +1,48 @@
 package com.claudiowork.algafood.di.notificacao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.claudiowork.algafood.di.modelo.Cliente;
 
+@Service
 public class AtivacaoClienteService {
 
 	//da para adicionar a opcao required com false quando nao quiser que a instancia nao seja feita
 	@Autowired//(required = false)
-	private Notificador notificador;
+	private List<Notificador> notificadores;
+	//private Notificador notificador;
 
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		//implementacao dupla por email e sms com a solução: updating the consumer to accept multiple beans
+		for (Notificador notificador: notificadores) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		}
+	}
+
+
+/*
+ * Anotações de estudo
+ * 
+ * quando apresentou o erro 
+ * 
+ * Description:
+
+Field notificador in com.claudiowork.algafood.di.notificacao.AtivacaoClienteService required a single bean, but 2 were found:
+	- notificadorEmail: defined in file [D:\1 - Projetos\Algaworks\EspecialistaRestAPI\EspecialistaRestAPI\AlgaFoodApi\target\classes\com\claudiowork\algafood\di\notificacao\NotificadorEmail.class]
+	- notificadorSMS: defined in file [D:\1 - Projetos\Algaworks\EspecialistaRestAPI\EspecialistaRestAPI\AlgaFoodApi\target\classes\com\claudiowork\algafood\di\notificacao\NotificadorSMS.class]
+
+
+Action:
+
+Consider marking one of the beans as @Primary, updating the consumer to accept multiple beans, or using @Qualifier to identify the bean that should be consumed
+
+consideramos que temos 2 beans para gerenciar assim podemos tomar varias atitudes para resolver e tratar a ambiguidade
+ * */	
+	
 //	@Autowired
 //	public AtivacaoClienteService(Notificador notificador) {
 //		this.notificador = notificador;
@@ -18,16 +51,7 @@ public class AtivacaoClienteService {
 
 //	public AtivacaoClienteService(String qualquer) {
 //		
-//	}
-	
-	public void ativar(Cliente cliente) {
-		cliente.ativar();
-		if (notificador != null)
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-		else
-			System.out.println("Não existe notificador, mas cliente foi ativado");
-	}
-
+//	}	
 //	@Autowired
 //	public void setNotificador(Notificador notificador) {
 //		this.notificador = notificador;
