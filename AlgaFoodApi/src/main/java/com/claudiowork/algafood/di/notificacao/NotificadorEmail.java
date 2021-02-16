d96f728716f1c60968d245b71b919e47b51e72bd
Claudio <claudiosfcarvalho@outlook.com>
2021-02-16 17:34:59 -0300

    @Value
    trabalhando com propriedades

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/di/notificacao/NotificadorEmail.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/di/notificacao/NotificadorEmail.java
@@ -1,22 +1,27 @@
 package com.claudiowork.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.claudiowork.algafood.di.modelo.Cliente;
 
-@Profile("prod")
+@Profile("pro")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 //@Primary //utilizado para desambiguar e coloca este como primeiro bean
 //@Qualifier("email")
 @Component
 public class NotificadorEmail implements Notificador {
-
+	@Value("${notificador.email.host-servidor}")
+	String host;
+	@Value("${notificador.email.porta-servidor}")
+	Integer porta;
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		String nome = cliente.getNome();
 		String email = cliente.getEmail();
-		
+		System.out.printf("Conectado no host %s:%d\n",host,porta);
 		System.out.printf("Notificação %s através do e-mail %s: %s\n",
 				nome, email, mensagem);
 	}
