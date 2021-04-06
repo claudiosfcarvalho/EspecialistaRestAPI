027facdf39a0322ada71faafd622901de4a43d52
Claudio <claudiosfcarvalho@outlook.com>
2021-04-06 08:21:22 -0300

    JSON: personalizando retorno JsonIgnore, JsonProperty e JsonRootName

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/domain/model/Cozinha.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/domain/model/Cozinha.java
@@ -6,9 +6,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@JsonRootName("gastronomia") //o mesmo que JsonProperty mas para o nome do objeto, visivel quando for xml
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -19,6 +24,8 @@ public class Cozinha {
 	@EqualsAndHashCode.Include
 	long id;
 	
+	//@JsonIgnore //não mostra o campo no retorno
+	@JsonProperty(value = "titulo") //define o nome do campo quando for serializado
 	@Column(nullable = false)
 	String nome;
 }
