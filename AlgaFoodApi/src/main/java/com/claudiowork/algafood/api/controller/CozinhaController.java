77c50ec21ea033fd996b1574016ab4c7588f8d96
Claudio <claudiosfcarvalho@outlook.com>
2021-04-08 08:02:25 -0300

    feat: trabalhando com ResponseEntity

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/controller/CozinhaController.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,10 @@ package com.claudiowork.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,8 +39,19 @@ public class CozinhaController {
     }
     
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable Long id) {
-    	return cozinhaRepository.buscar(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+    	Cozinha c = cozinhaRepository.buscar(id);
+    	return ResponseEntity.ok(c);
     }
-
+    
+    @GetMapping("/testeLocation/{id}")
+    public ResponseEntity<Cozinha> testeLocation(@PathVariable Long id) {
+    	Cozinha c = cozinhaRepository.buscar(id);
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+    	return ResponseEntity
+    			.status(HttpStatus.FOUND)
+    			.headers(headers)
+    			.build();
+    }
 }
