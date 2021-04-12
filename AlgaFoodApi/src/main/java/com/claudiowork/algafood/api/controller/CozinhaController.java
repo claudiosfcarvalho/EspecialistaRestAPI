4036c91f8f9465b8646bebf6895b49310b5a6132
Claudio <claudiosfcarvalho@outlook.com>
2021-04-12 08:53:29 -0300

    feat: criando camada service e tratando com exception

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/controller/CozinhaController.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.claudiowork.algafood.api.model.CozinhasXmlWrapper;
+import com.claudiowork.algafood.domain.exception.EntidadeEmUsoException;
+import com.claudiowork.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.claudiowork.algafood.domain.model.Cozinha;
-import com.claudiowork.algafood.domain.repository.CozinhaRepository;
+import com.claudiowork.algafood.domain.service.CadastroCozinhaService;
 
 @RestController
 @RequestMapping(value = "/cozinhas") // , produces = MediaType.APPLICATION_JSON_VALUE
 public class CozinhaController {
 
 	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	private CadastroCozinhaService cozinhaService;
 
 	/*
 	 * content negotiation - é possível especificar um ou mais tipos de conteúdo
@@ -38,17 +39,17 @@ public class CozinhaController {
 	 */
 	@GetMapping
 	public List<Cozinha> listar() {
-		return cozinhaRepository.listar();
+		return cozinhaService.listar();
 	}
 
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
-	}
+//	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+//	public CozinhasXmlWrapper listarXml() {
+//		return new CozinhasXmlWrapper(cozinhaService.listar());
+//	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
-		Cozinha c = cozinhaRepository.buscar(id);
+		Cozinha c = cozinhaService.buscar(id);
 		if (c != null) {
 			return ResponseEntity.ok(c);
 		}
@@ -57,39 +58,31 @@ public class CozinhaController {
 
 	@PostMapping
 	public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
-		Cozinha c = cozinhaRepository.salvar(cozinha);
+		Cozinha c = cozinhaService.salvar(cozinha);
 		return ResponseEntity.status(HttpStatus.CREATED).body(c);
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
-		Cozinha c = cozinhaRepository.buscar(id);
+		Cozinha c = cozinhaService.buscar(id);
 		if (c != null) {
-			BeanUtils.copyProperties(c, cozinha, "id");
-			cozinhaRepository.salvar(cozinha);
+			BeanUtils.copyProperties(cozinha, c, "id");
+			cozinha = cozinhaService.salvar(c);
 			return ResponseEntity.status(HttpStatus.OK).body(c);
 		}
 		return ResponseEntity.notFound().build();
 	}
 
-//	@GetMapping("/testeLocation/{id}")
-//	public ResponseEntity<Cozinha> testeLocation(@PathVariable Long id) {
-//		Cozinha c = cozinhaRepository.buscar(id);
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-//		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
-//	}
-	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
 		try {
-			Cozinha c = cozinhaRepository.buscar(id);
-			if (c != null) {
-				cozinhaRepository.remover(c);
-				return ResponseEntity.noContent().build();
-			}
+			cozinhaService.remover(id);
+			return ResponseEntity.noContent().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
-		} catch (DataIntegrityViolationException e) {
+
+		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
