1af83e385ae8a51c1cda1989208886903beaddc9
Claudio <claudiosfcarvalho@outlook.com>
2021-04-16 08:08:58 -0300

    feat: implementando http Patch, ReflectionUtils, ObjectMapper

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/controller/RestauranteController.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/controller/RestauranteController.java
@@ -1,13 +1,17 @@
 package com.claudiowork.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,6 +23,7 @@ import com.claudiowork.algafood.domain.exception.EntidadeEmUsoException;
 import com.claudiowork.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.claudiowork.algafood.domain.model.Restaurante;
 import com.claudiowork.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
@@ -45,19 +50,32 @@ public class RestauranteController {
 
 	@PostMapping
 	public ResponseEntity<Restaurante> adicionar(@RequestBody Restaurante restaurante) {
-		Restaurante c = restauranteService.salvar(restaurante);
-		return ResponseEntity.status(HttpStatus.CREATED).body(c);
+		restaurante = restauranteService.salvar(restaurante);
+		return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Restaurante restaurante) {
 		try {
-			Restaurante c = restauranteService.buscar(id);
-			if (c != null) {
+			Restaurante restAtual = restauranteService.buscar(id);
+			if (restAtual != null) {
+				BeanUtils.copyProperties(restaurante, restAtual, "id");
+				restaurante = restauranteService.salvar(restAtual);
+				return ResponseEntity.status(HttpStatus.OK).body(restAtual);
+			}
+			return ResponseEntity.notFound().build();
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 
-				BeanUtils.copyProperties(restaurante, c, "id");
-				restaurante = restauranteService.salvar(c);
-				return ResponseEntity.status(HttpStatus.OK).body(c);
+	@PatchMapping("/{id}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
+		try {
+			Restaurante restAtual = restauranteService.buscar(id);
+			if (restAtual != null) {
+				merge(campos, restAtual);
+				return atualizar(id, restAtual);
 			}
 			return ResponseEntity.notFound().build();
 		} catch (EntidadeNaoEncontradaException e) {
@@ -79,4 +97,20 @@ public class RestauranteController {
 		}
 	}
 
+	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
+		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			// busca o campo na entidade
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+
+			// permite acessar o atributo privado do Objeto Restaurante
+			field.setAccessible(true);
+
+			//obtem o valor convertido no tipo correto. ex de integer para bigdecimal
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
+	}
 }
