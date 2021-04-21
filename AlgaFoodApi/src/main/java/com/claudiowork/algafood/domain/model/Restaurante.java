16d880669b3439f593b44249357cabff01d04189
Claudio <claudiosfcarvalho@outlook.com>
2021-04-21 20:30:39 -0300

    feat: mapeando ManyToMany

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/domain/model/Restaurante.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.claudiowork.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,7 +10,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -32,4 +38,15 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)//forçar um nome específico para coluna para fk
 	private Cozinha cozinha;
+	
+	/**
+	 * joincolumns define qual coluna tem relacao a referente da classe mapeada
+	 * inverseJoinColumns referencia qual coluna da outra tabela tem relacao
+	 */
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
--- AlgaFoodApi/src/main/resources/import.sql
+++ AlgaFoodApi/src/main/resources/import.sql
@@ -1,5 +1,6 @@
 insert into cozinha (id,nome) values (1,'Tailandesa');
 insert into cozinha (id,nome) values (2,'Japonesa');
+insert into cozinha (id,nome) values (3,'Chinesa');
 
 insert into restaurante (id,nome, taxa_frete, cozinha_id) values (1,'Thai Gourmet', 10, 1);
 insert into restaurante (id,nome,taxa_frete,cozinha_id) values (2,'Gendai',8,2);
@@ -21,4 +22,6 @@ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_cozinhaS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_cozinhaS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1,2), (1, 3), (2,1), (3,2), (3,3);
     
