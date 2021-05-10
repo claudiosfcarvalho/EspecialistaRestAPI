b30904734ef5558af522a3d7ef51eaadffbd66c4
Claudio <claudiosfcarvalho@outlook.com>
2021-05-10 08:52:13 -0300

    feat: implementando novos tratamentos de erros e personalizando padrões de mensagem

--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,44 +2,54 @@ package com.claudiowork.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.claudiowork.algafood.domain.exception.EntidadeEmUsoException;
 import com.claudiowork.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.claudiowork.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e,
+			WebRequest request) {
+
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+
 	}
-	
+
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	public ResponseEntity<?> tratarNegocioException(NegocioException e, WebRequest request) {
+
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
 	}
-	
+
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+
 	}
-	
-	
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		body = Problema
+				.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(body == null || !(body instanceof String) ? status.getReasonPhrase() : ex.getMessage())
+				.build();
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+
 }
--- AlgaFoodApi/src/main/java/com/claudiowork/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ AlgaFoodApi/src/main/java/com/claudiowork/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -4,7 +4,6 @@ public class EntidadeNaoEncontradaException extends NegocioException {
 
 	private static final long serialVersionUID = 1L;
 
-	
 	public EntidadeNaoEncontradaException(String mensagem) {
 		super(mensagem);
 	}
